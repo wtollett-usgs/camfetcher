7494cc7685cb61eeef722e30b86841ee1cf49ea9
Tom Parker <tparker@usgs.gov>
2018-08-02 15:41:58 -0800

    extract util functions

--- camfetcher.py
+++ camfetcher.py
@@ -12,7 +12,6 @@
 import logging
 import os
 import pathlib
-from buffering_smtp_handler import BufferingSMTPHandler
 from imaplib import IMAP4, IMAP4_SSL
 import email
 import email.message
@@ -20,52 +19,12 @@ import email.policy
 import sys
 import shutil
 import time
-
+from camutil import *
 
 REQ_VERSION = (3, 5)
 TMP_DIR = "/tmp"
 
 env = None
-
-
-def exit_with_error(error):
-    logger.error(error)
-    logging.shutdown()
-    sys.exit(1)
-
-
-def get_env_var(var, default=None):
-    if var in os.environ:
-        logger.debug("%s: %s", var, os.environ[var])
-        return os.environ[var]
-
-    else:
-        if default is None:
-            msg = "Envionment variable {} not set, exiting.".format(var)
-            exit_with_error(EnvironmentError(msg))
-        else:
-            logger.debug("%s: %s (default)", var, default)
-            return default
-
-
-def setup_logging():
-    global logger
-    logger = logging.getLogger("")
-    logger.setLevel(logging.DEBUG)
-
-    ch = logging.StreamHandler()
-    logger.addHandler(ch)
-
-    try:
-        subject = "camfetcher logs"
-        handler = BufferingSMTPHandler(os.environ['MAILHOST'],
-                                       os.environ['FF_SENDER'],
-                                       os.environ['FF_RECIPIENT'], subject,
-                                       1000, "%(levelname)s - %(message)s")
-        handler.setLevel(logging.ERROR)
-        logger.addHandler(handler)
-    except KeyError:
-        logger.info("SMTP logging not configured.")
 
 
 def get_archive_dir(cam, image_time):
